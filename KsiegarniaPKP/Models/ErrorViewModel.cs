4a62fab52b97c04a87cf8e541b1bafaa9020ef74
Szymon Wieczorek <doszymona@gmail.com>
2024-01-16 13:12:23 +0100

    Project init

---
+++ KsiegarniaPKP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace KsiegarniaPKP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
