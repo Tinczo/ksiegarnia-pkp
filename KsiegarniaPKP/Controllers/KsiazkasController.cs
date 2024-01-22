bd30c069ef2fd60e7583de3c08700e292cc5e9e5
Karol Pilat <karolpilat59@gmail.com>
2024-01-22 20:05:36 +0100

    Adjust books offer view

--- KsiegarniaPKP/Controllers/KsiazkasController.cs
+++ KsiegarniaPKP/Controllers/KsiazkasController.cs
@@ -6,16 +6,22 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using KsiegarniaPKP.Models;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Globalization;
 
 namespace KsiegarniaPKP.Controllers
 {
     public class KsiazkasController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly ILogger<KsiazkasController> _logger;
 
-        public KsiazkasController(ApplicationDbContext context)
+        public KsiazkasController(ApplicationDbContext context, ILogger<KsiazkasController> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
         // GET: Ksiazkas
@@ -53,15 +59,36 @@ namespace KsiegarniaPKP.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Autor,Tytul,RokWydania,Opis,Cena")] Ksiazka ksiazka)
+        public async Task<IActionResult> Create([Bind("Id,Autor,Tytul,RokWydania,Opis,Cena,Obrazek")] KsiazkaCreateView ksiazkaView)
         {
             if (ModelState.IsValid)
             {
+                var ksiazka = new Ksiazka
+                {
+                    Id = ksiazkaView.Id,
+                    Autor = ksiazkaView.Autor,
+                    Tytul = ksiazkaView.Tytul,
+                    RokWydania = ksiazkaView.RokWydania,
+                    Opis = ksiazkaView.Opis,
+                    Cena = ksiazkaView.Cena,
+                };
+
+                if (ksiazkaView.Obrazek != null && ksiazkaView.Obrazek.Length > 0)
+                {
+                    var fileName = Path.GetFileName(ksiazkaView.Obrazek.FileName);
+                    var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/book-images", fileName);
+                    using (var fileSteam = new FileStream(filePath, FileMode.Create))
+                    {
+                        await ksiazkaView.Obrazek.CopyToAsync(fileSteam);
+                    }
+                    ksiazka.ObrazekUrl = "/book-images/" + fileName; // Zapisz ścieżkę obrazka
+                }
+
                 _context.Add(ksiazka);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            return View(ksiazka);
+            return View(ksiazkaView);
         }
 
         // GET: Ksiazkas/Edit/5
@@ -77,7 +104,18 @@ namespace KsiegarniaPKP.Controllers
             {
                 return NotFound();
             }
-            return View(ksiazka);
+
+            var ksiazkaView = new KsiazkaEditView
+            {
+                Id = ksiazka.Id,
+                Autor = ksiazka.Autor,
+                Tytul = ksiazka.Tytul,
+                RokWydania = ksiazka.RokWydania,
+                Opis = ksiazka.Opis,
+                Cena = ksiazka.Cena,
+            };
+
+            return View(ksiazkaView);
         }
 
         // POST: Ksiazkas/Edit/5
@@ -85,9 +123,9 @@ namespace KsiegarniaPKP.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Autor,Tytul,RokWydania,Opis,Cena")] Ksiazka ksiazka)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Autor,Tytul,RokWydania,Opis,Cena,Obrazek")] KsiazkaEditView ksiazkaView)
         {
-            if (id != ksiazka.Id)
+            if (id != ksiazkaView.Id)
             {
                 return NotFound();
             }
@@ -96,12 +134,35 @@ namespace KsiegarniaPKP.Controllers
             {
                 try
                 {
+                    var ksiazka = await _context.Ksiazki.FirstOrDefaultAsync(k => k.Id == ksiazkaView.Id);
+                    if (ksiazka == null)
+                    {
+                        return NotFound();
+                    }
+
+                    ksiazka.Autor = ksiazkaView.Autor;
+                    ksiazka.Tytul = ksiazkaView.Tytul;
+                    ksiazka.RokWydania = ksiazkaView.RokWydania;
+                    ksiazka.Opis = ksiazkaView.Opis;
+                    ksiazka.Cena = ksiazkaView.Cena;
+
+                    if (ksiazkaView.Obrazek != null && ksiazkaView.Obrazek.Length > 0)
+                    {
+                        var fileName = Path.GetFileName(ksiazkaView.Obrazek.FileName);
+                        var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/book-images", fileName);
+                        using (var fileSteam = new FileStream(filePath, FileMode.Create))
+                        {
+                            await ksiazkaView.Obrazek.CopyToAsync(fileSteam);
+                        }
+                        ksiazka.ObrazekUrl = "/book-images/" + fileName;
+                    }
+
                     _context.Update(ksiazka);
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!KsiazkaExists(ksiazka.Id))
+                    if (!KsiazkaExists(ksiazkaView.Id))
                     {
                         return NotFound();
                     }
@@ -112,8 +173,9 @@ namespace KsiegarniaPKP.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            return View(ksiazka);
+            return View(ksiazkaView);
         }
+
 
         // GET: Ksiazkas/Delete/5
         public async Task<IActionResult> Delete(int? id)
--- KsiegarniaPKP/Models/Ksiazka.cs
+++ KsiegarniaPKP/Models/Ksiazka.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace KsiegarniaPKP.Models
@@ -8,18 +9,73 @@ namespace KsiegarniaPKP.Models
         [Key]
         public int Id { get; set; }
 
-        [Required, MaxLength(255)]
+        [Required, MaxLength(100)]
         public string Autor { get; set; }
 
-        [Required, MaxLength(255)]
+        [Required, MaxLength(100)]
         public string Tytul { get; set; }
 
         public int RokWydania { get; set; }
 
-        [Required, MaxLength(255)]
+        [Required, MaxLength(500)]
         public string Opis { get; set; }
         [Required]
+        [DataType(DataType.Currency)]
+        [DisplayFormat(DataFormatString = "{0:F2}", ApplyFormatInEditMode = true)]
         public float Cena { get; set; }
+
+        [Required, MaxLength(255)]
+        public string ObrazekUrl { get; set; }
+
+        public virtual List<Oferta> Oferty { get; set; } = new List<Oferta>();
+    }
+
+    public class KsiazkaCreateView
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Autor { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Tytul { get; set; }
+
+        public int RokWydania { get; set; }
+
+        [Required, MaxLength(500)]
+        public string Opis { get; set; }
+        [Required]
+        [DataType(DataType.Currency)]
+        [DisplayFormat(DataFormatString = "{0:F2}", ApplyFormatInEditMode = true)]
+        public float Cena { get; set; }
+
+        [Required]
+        public IFormFile Obrazek { get; set; }
+
+        public virtual List<Oferta> Oferty { get; set; } = new List<Oferta>();
+    }
+
+    public class KsiazkaEditView
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Autor { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Tytul { get; set; }
+
+        public int RokWydania { get; set; }
+
+        [Required, MaxLength(500)]
+        public string Opis { get; set; }
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:F2}", ApplyFormatInEditMode = true)]
+        public float Cena { get; set; }
+
+        public IFormFile Obrazek { get; set; }
 
         public virtual List<Oferta> Oferty { get; set; } = new List<Oferta>();
     }
